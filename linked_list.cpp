f6f94e22ba90515be9fa144b6cb00b38d4e67bd2
= <=daman.4880@gmail.com>
2015-11-12 19:58:10 +0530

    added linked_list

--- linked_list.cpp
+++ linked_list.cpp
@@ -11,6 +11,9 @@ void traverse();
 void insert_at_begin(int n);
 void insert_at_end(int n);
 void insert_after_given_node(int n,int loc);
+void delete_at_begin();
+void delete_at_end();
+void delete_after_given_node(int n);
 node *head = NULL;
 
 int main()
@@ -19,11 +22,14 @@ int main()
     while(true)
     {
         cout<<"Enter one of following operations: "<<endl;
-        cout<<"1 to insert element at beginning\n";
+        cout<<"1 to delete element at beginning\n";
         cout<<"2 to insert element at end\n";
         cout<<"3 to insert after given node\n";
-        cout<<"4 to view the list by traversing\n";
-        cout<<"5 to exit\n";
+        cout<<"4 to delete element at beginning\n";
+        cout<<"5 to delete element at end\n";
+        cout<<"6 to delete element after\n";
+        cout<<"7 to view the list by traversing\n";
+        cout<<"8 to exit\n";
         cin>>x;
 
         switch(x){
@@ -41,10 +47,19 @@ int main()
                         cin>>l;
                         insert_after_given_node(y,l);
                         break;
-            case  4:    traverse();
+            case  7:    traverse();
                         break;
-            case  5:    cout<<"Goodbye"<<endl;
+            case  4:    delete_at_begin();
+                        break;
+            case  6:    cout<<"Enter location of node which is to be deleted:";
+                        cin>>l;
+                        delete_after_given_node(l);
+                        break;
+            case  5:    delete_at_end();
+                        break;
+            case  8:    cout<<"Goodbye"<<endl;
                         return 0;
+
             default :   cout<<"Please enter valid operation code.\n";
                         break;
         }
@@ -127,5 +142,44 @@ void insert_after_given_node(int n,int loc)
         }
         ptr = ptr->next;
     }
+}
+void delete_at_begin()
+{
+    node *ptr = head;
+    node *temp2 = ptr->next;
+    delete ptr;
+    head = temp2;
+
+}
+void delete_at_end()
+{
+    node *ptr = head;
+    node *temp = NULL;
+    while(ptr->next != NULL)
+    {
+        temp = ptr;
+        ptr = ptr->next;
+    }
+    delete ptr;
+    temp->next = NULL;
+}
+void delete_after_given_node(int n)
+{
+    int c = 0;
+    node *ptr = head;
+    node *temp = NULL;
+    while(ptr->next !=NULL)
+    {
+        c++;
+        temp = ptr;
+        ptr = ptr->next;
+        if(c == n)
+        {
+            break;
+        }
+
+    }
+    temp->next = ptr->next;
+    delete ptr;
 
 }
